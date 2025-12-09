1421280eff674bb780052e86fb99e5fe5b0f25ef
GalaxyVinci05 <vincenzopaolo0@gmail.com>
2025-12-09 00:58:14 +0000

    added passport for auth, started dashboard

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config({ quiet: true });
 
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
+import session from "express-session";
+import passport, { serializeUser } from "passport";
+import { Strategy as twitchStrategy } from "passport-twitch-latest";
+import { v4 as uuid } from "uuid";
 import path from 'path';
 import querystring from "querystring";
 import superagent from "superagent";
@@ -18,53 +22,31 @@ export = class {
         server.use('/public', express.static(path.resolve(__dirname, '../dashboard/public')));
         server.use(express.json());
         server.use(express.urlencoded({ extended: false }));
+        server.use(
+            session({
+                genid: () => {
+                    return uuid();
+                },
+                secret: process.env.SESSION_SECRET,
+                resave: false,
+                saveUninitialized: false
+            }),
+        );
+        server.use(passport.initialize());
+        server.use(passport.session());
         server.set('views', path.join(__dirname, '../dashboard/views'));
 
         server.set('view engine', 'ejs');
 
-        const renderView = (req: Request, res: Response, view: string, data: any = {}) => {
-            const baseData = {
-                bot: client,
-                path: req.path,
-            };
-            res.render(path.resolve(`./dashboard/views/${view}`), Object.assign(baseData, data));
-        };
-
-        server.get('/', (req, res) => {
-            renderView(req, res, 'index');
-        });
-
-        server.get('/auth', (req, res) => {
-            let url = 'https://id.twitch.tv/oauth2/authorize?' + querystring.stringify({
-                client_id: client.config.clientId,
-                redirect_uri: process.env.REDIRECT_URI,
-                response_type: 'code',
+        passport.use(
+            new twitchStrategy({
+                clientID: client.config.clientId,
+                clientSecret: client.config.clientSecret,
+                callbackURL: process.env.REDIRECT_URI,
                 scope: 'chat:read chat:edit'
-            });
-
-            res.redirect(url);
-        });
-
-        server.get('/auth/callback', async (req, res) => {
-            let data = querystring.stringify({
-                client_id: client.config.clientId,
-                client_secret: client.config.clientSecret,
-                code: req.query.code as string,
-                grant_type: 'authorization_code',
-                redirect_uri: process.env.REDIRECT_URI,
-            });
-
-            try {
-                let auth = await superagent
-                    .post('https://id.twitch.tv/oauth2/token')
-                    .set('Content-Type', 'application/x-www-form-urlencoded')
-                    .send(data);
-
-                let { access_token } = auth.body;
-                let user = await getUserByToken(access_token);
-
-                let channelName = user.data[0].login;
-                let channelId = user.data[0].id;
+            }, async (accessToken, refreshToken, profile, done) => {
+                let channelName = profile.login;
+                let channelId = profile.id;
 
                 let channels: User[] = channelsdb.get("channels");
                 let channel: User = channels.find(c => c.userId == channelId);
@@ -87,12 +69,112 @@ export = class {
                     await client.join(channelName);
                 }
 
-                renderView(req, res, 'authsuccess');
-            } catch (err) {
-                client.logger.error('Error exchanging code for access token:', err);
-                renderView(req, res, 'autherror');
-            }
+                let user: User = {
+                    userId: profile.id,
+                    userName: profile.login
+                };
+                done(null, user);
+            })
+        );
+
+        passport.serializeUser((user: User, done) => {
+            done(null, user.userId);
         });
+
+        passport.deserializeUser((userId, done) => {
+            let channels: User[] = channelsdb.get("channels");
+            let user = channels.find(c => c.userId == userId);
+
+            done(null, user);
+        });
+
+        const renderView = (req: Request, res: Response, view: string, data: any = {}) => {
+            const baseData = {
+                bot: client,
+                path: req.path,
+            };
+            res.render(path.resolve(`./dashboard/views/${view}`), Object.assign(baseData, data));
+        };
+
+        server.get('/', (req, res) => {
+            console.log(req.isAuthenticated());
+            console.log(req.user);
+            renderView(req, res, 'index');
+        });
+
+        server.get('/auth', passport.authenticate('twitch'));
+        server.get('/auth/callback', passport.authenticate('twitch', {
+            successRedirect: '/dashboard',
+            failureRedirect: '/auth/error'
+        }));
+
+        server.get('/auth/success', (req, res) => {
+            renderView(req, res, 'authsuccess');
+        });
+
+        server.get('/auth/error', (req, res) => {
+            renderView(req, res, 'autherror');
+        });
+
+        //server.get('/auth', (req, res) => {
+        //    let url = 'https://id.twitch.tv/oauth2/authorize?' + querystring.stringify({
+        //        client_id: client.config.clientId,
+        //        redirect_uri: process.env.REDIRECT_URI,
+        //        response_type: 'code',
+        //        scope: 'chat:read chat:edit'
+        //    });
+
+        //    res.redirect(url);
+        //});
+
+        //server.get('/auth/callback', async (req, res) => {
+        //    let data = querystring.stringify({
+        //        client_id: client.config.clientId,
+        //        client_secret: client.config.clientSecret,
+        //        code: req.query.code as string,
+        //        grant_type: 'authorization_code',
+        //        redirect_uri: process.env.REDIRECT_URI,
+        //    });
+
+        //    try {
+        //        let auth = await superagent
+        //            .post('https://id.twitch.tv/oauth2/token')
+        //            .set('Content-Type', 'application/x-www-form-urlencoded')
+        //            .send(data);
+
+        //        let { access_token } = auth.body;
+        //        let user = await getUserByToken(access_token);
+
+        //        let channelName = user.data[0].login;
+        //        let channelId = user.data[0].id;
+
+        //        let channels: User[] = channelsdb.get("channels");
+        //        let channel: User = channels.find(c => c.userId == channelId);
+
+        //        if (!channel) {
+        //            // push to channels db
+        //            channels.push({ userId: channelId, userName: channelName });
+        //            await channelsdb.set("channels", channels);
+
+        //            await client.join(channelName);
+        //            await client.db.setDefault({ channelId, channelName });
+
+        //            await client.say(channelName, "Thanks for adding gdreqbot! You can get a list of commands by typing !help");
+        //            client.logger.log(`→   New channel joined: ${channelName}`);
+        //        } else if (channel.userName != channelName) {
+        //            let idx = channels.findIndex(c => c.userId == channelId);
+        //            channels[idx].userName = channelName;
+
+        //            await channelsdb.set("channels", channels);
+        //            await client.join(channelName);
+        //        }
+
+        //        renderView(req, res, 'authsuccess');
+        //    } catch (err) {
+        //        client.logger.error('Error exchanging code for access token:', err);
+        //        renderView(req, res, 'autherror');
+        //    }
+        //});
 
         server.get('/stats', (req, res) => {
             renderView(req, res, 'stats');
@@ -110,6 +192,39 @@ export = class {
             renderView(req, res, 'privacy-policy');
         });
 
+        server.get('/dashboard', (req, res) => {
+            if (req.isAuthenticated())
+                return res.redirect(`/dashboard/${(req.user as User).userId}`);
+
+            res.redirect('/auth');
+        });
+
+        server.get('/dashboard/:user', this.checkAuth, (req, res) => {
+            if ((req.user as User).userId != req.params.user)
+                return res.status(403).send('Unauthorized');
+
+            res.render('dashboard', {
+                isAuthenticated: true,
+                user: req.user
+            });
+        });
+
+        server.get('/logout', (req, res, next) => {
+            if (req.isAuthenticated())
+                req.logout(err => {
+                    if (err) return next(err);
+                });
+
+            res.redirect('/');
+        });
+
         client.server = server.listen(parseInt(port.toString()), hostname, () => console.log(`Server listening on http(s)://${hostname}:${port}`));
+    }
+
+    checkAuth(req: Request, res: Response, next: NextFunction) {
+        if (req.isAuthenticated())
+            return next();
+        
+        res.redirect('/dashboard');
     }
 }
